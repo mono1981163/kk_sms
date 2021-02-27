b87bb6e90cc112c68abc855efc8d9be735067d14
bear_star713 <bear_star713@hotmail.com>
2021-02-27 22:20:50 +0900

    inventory

---
+++ kk_sms/inventoryManagement/Form_ChangePurchaser.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace kk_sms.inventoryManagement
+{
+    partial class Form_ChangePurchaser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Indigo;
+            this.label1.Font = new System.Drawing.Font("Meiryo", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(110, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "在庫品仕入担当者変更";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form_ChangePurchaser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 519);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Meiryo", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form_ChangePurchaser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_ChangePurchaser";
+            this.ResumeLayout(false);
+
+
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Data.DataTable purchaserTable;
+        private System.Data.DataColumn slipnumber;
+        private System.Data.DataColumn productname;
+        private System.Data.DataColumn employeenumber;
+        private System.Data.DataColumn employeename;
+        private System.Data.DataColumn supplier;
+        private System.Data.DataColumn date;
+
+    }
+}
---
+++ kk_sms/inventoryManagement/Form_ChangePurchaser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kk_sms.inventoryManagement
+{
+    public partial class Form_ChangePurchaser : Form
+    {
+        public Form_ChangePurchaser()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
